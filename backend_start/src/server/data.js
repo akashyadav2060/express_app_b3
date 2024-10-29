2a65366cc2dd76c6d626d185fcdc39715e749d5b
akashyadav2060 <yadavakash2060@gmail.com>
2024-10-30 02:27:57 +1100

    created api

---
+++ backend_start/src/server/data.js
@@ -0,0 +1,13 @@
+export const students=[
+    {id: 1,
+        firstName: 'Alex',
+  lastName:'shah',
+  email: 'alex@gmail.com',
+mobile:'080293742'},
+{ id: 2,
+  firstName: 'Donald',
+  lastName: 'Duck',
+  email:'duckd2gmail.com',
+  mobile:'9868765159'
+}
+  ];
